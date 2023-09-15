b261f371dad6bf2f68d51c57b8887d724bc05606
TURMA-A <angelovinicius123@gmail.com>
2023-09-15 08:53:44 -0300

    Fiz o exercicio06.py

---
+++ Aula01/exercicio06.py
@@ -0,0 +1,27 @@
+# # Forma de fazer com tuple
+#
+# A = 5
+# B = 10
+#
+# print(f"Esse é o valor de A: {A}"
+#       f"\nE esse é o valor de B: {B}")
+#
+# A, B = B, A
+#
+# print(f"\nEsse é o novo valor de A: {A}"
+#       f"\nE esse é o novo valor de B: {B}")
+#
+# # Forma de fazer com o temp
+# A = 5
+# B = 10
+#
+# print(f"Esse é o valor de A: {A}"
+#       f"\nE esse é o valor de B: {B}")
+#
+# temp = A
+# A = B
+# B = temp
+#
+# print(f"\nEsse é o novo valor de A: {A}"
+#       f"\nE esse é o novo valor de B: {B}")
+
