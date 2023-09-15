9a906e525bd7ab910ad2694a83d285f2b1ac2b75
TURMA-A <angelovinicius123@gmail.com>
2023-09-15 09:06:25 -0300

    Fiz o exercicio02.py

--- Aula01/exercicio02.py
+++ Aula01/exercicio02.py
@@ -1,9 +1,8 @@
-#Variáveis
+# Variáveis
 num = int(input("Digite um número: "))
 num1 = int(input("Digite um número: "))
 soma = num + num1
 
-#Print
+# Print
 print(f"Estes são os seus números: {num} e {num1}")
-
 print(f"Este é o valor da sua soma: {soma}")
