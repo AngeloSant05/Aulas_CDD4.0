8f6403c43c5eebc82a977f3869ec31f094116e36
angel <angelovinicius123@gmail.com>
2023-09-15 18:07:04 -0300

    Fiz o exercício01.py/casa

---
+++ Aula04/Casa/exercicio01.py
@@ -0,0 +1,27 @@
+contador_posi = 10
+contador_nega = 0
+soma_nega = 0
+
+for x in range(0, 10):
+    num = int(input("Digite um número: "))
+    if num < 0:
+        contador_nega += 1
+        print(f"{num} é um número negativo.")
+        soma_nega += num
+        contador_posi -= 1
+if contador_nega == 0:
+    print(f"Não ouve números negativos")
+else:
+    if contador_nega == 1:
+        print(f"Tivemos um total de {contador_nega} número negativo")
+        print(f"E esse foi o número: {soma_nega}")
+    else:
+        print(f"Tivemos um total de {contador_nega} números negativos")
+        print(f"E essa foi a soma dos números negativos: {soma_nega}")
+if contador_posi == 0:
+    print(f"Não ouve números positivos")
+else:
+    if contador_posi == 1:
+        print(f"Tivemos um total de {contador_posi} número positivo")
+    else:
+        print(f"Tivemos um total de {contador_posi} números positivos")
