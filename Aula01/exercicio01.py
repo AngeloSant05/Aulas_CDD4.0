c63a0806af128b51d5a7c6b1e374551cd53c623d
TURMA-A <angelovinicius123@gmail.com>
2023-09-15 09:05:37 -0300

    Fiz o exercicio01.py

--- Aula01/exercicio01.py
+++ Aula01/exercicio01.py
@@ -1,16 +1,13 @@
-#Variáveis
+# Variáveis
 usuario = input("Digite o seu nome de usuário: ")
 usuario1 = input("Digite o seu nome de usuário: ")
 usuario2 = input("Digite o seu nome de usuário: ")
 
-#Print simples
+# Print simples
 print("Estes são seus usuários:", usuario, usuario1, usuario2)
 
-#Print formatado
+# Print formatado
 print(f"Olá alunos {usuario} {usuario1} {usuario2} parabéns")
 
-#Print formatado com quebra de linha
+# Print formatado com quebra de linha
 print(f"Olá alunos \n{usuario}\n{usuario1}\n{usuario2}\nparabéns")
-
-
-
