b5e3afe92fc76464d6daa0b0cb8ab584e1feeb5d
TURMA-A <angelovinicius123@gmail.com>
2023-09-15 08:53:28 -0300

    Fiz o exercicio05.py

---
+++ Aula01/exercicio05.py
@@ -0,0 +1,14 @@
+#Variáveis
+num1 = float(input("Digite um número: "))
+num2 = float(input("Digite outro número: "))
+soma = num1 + num2
+subtracao = num1 - num2
+multiply = num1 * num2
+div = num1 / num2
+
+#Print
+print(f"O valor dos números são: {num1},{num2}")
+print(f"Esse é o valor da soma dos dois números: {soma}"
+      f"\nEsse é o valor da subtração dos dois números: {subtracao}"
+      f"\nEsse é o valor da multiplicação dos dois números: {multiply}"
+      f"\nEsse é o valor da divisão dos dois números: {div}")
