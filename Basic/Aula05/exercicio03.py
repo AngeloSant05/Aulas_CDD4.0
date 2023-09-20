7adef3e302a1a0738e8327a4b99ff5b783e85a7e
TURMA-A <angelovinicius123@gmail.com>
2023-09-20 10:32:33 -0300

    Fiz o exercicio03py

--- Basic/Aula05/exercicio03.py
+++ Basic/Aula05/exercicio03.py
@@ -2,7 +2,6 @@ num = float(input("Insira um valor: "))
 num1 = float(input("Insira outro valor: "))
 
 while num1 == 0:
-    # print("O segundo valor não pode ser 0.")
     num1 = float(input("Valor inválido. \nInsira outro valor: "))
 div = num / num1
 print(div)
