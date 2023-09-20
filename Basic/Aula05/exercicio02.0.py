9246e3b9940ad2eb9903555acd3aead9ccbe80b7
TURMA-A <angelovinicius123@gmail.com>
2023-09-20 09:44:39 -0300

    Fiz o exercicio02.0py

---
+++ Basic/Aula05/exercicio02.0.py
@@ -0,0 +1,13 @@
+cont = 1
+soma = 0
+
+while cont <= 10:
+    num = float(input("Digite sua nota: "))
+    soma += num
+    cont += 1
+print(f"{soma / 10}")
+
+# Outra forma de calcular
+
+# media = soma / 10
+# print(media)
