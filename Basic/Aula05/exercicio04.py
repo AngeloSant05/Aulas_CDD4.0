2f29df33268c11c1026d0c38870b118c0ad647c4
TURMA-A <angelovinicius123@gmail.com>
2023-09-20 11:38:28 -0300

    Fiz o exercicio04.py

--- Basic/Aula05/exercicio04.py
+++ Basic/Aula05/exercicio04.py
@@ -1,9 +1,9 @@
 senha = "cdd4.0"
-senha_comp = input("Digite sua senha: ")
+senha_solic = input("Digite sua senha: ")
 cont = 1
 
-while senha != senha_comp:
-    senha_comp = input("Senha errada.\nTente novamente:")
+while senha != senha_solic:
+    senha_solic = input("Senha errada.\nTente novamente:")
     cont += 1
     if cont > 3:
         print("Número de tentativas alcançadas.")
