48a5944be3dc1fa9353d40dd40d322308c08113e
TURMA-A <angelovinicius123@gmail.com>
2023-09-22 08:19:35 -0300

    Fiz o exercicio04.0py

--- Basic/Aula05/exercicio04.0.py
+++ Basic/Aula05/exercicio04.0.py
@@ -1,12 +1,11 @@
 senha = "cdd4.0"
 senha_solic = input("Digite sua senha: ")
-cont = 1
+tentativas = 1
 
 while senha != senha_solic:
-    if cont >= 3:
+    if tentativas >= 3:
         print("Número de tentativas alcançadas.\n       Usuário bloqueado.")
         break
-    senha_solic = input("Senha errada.\nTente novamente: ")
-    cont += 1
+    tentativas += 1
 else:
     print("Senha correta")
