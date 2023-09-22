88a7bc720e77c8b4204ed8fdfb34ba1539a7cffe
TURMA-A <angelovinicius123@gmail.com>
2023-09-22 08:20:14 -0300

    Adicionei um contador ao exercicio04.0.py

---
+++ Basic/Aula05/exercicio04.1.py
@@ -0,0 +1,12 @@
+senha = "cdd4.0"
+senha_solic = input("Digite sua senha: ")
+tentativas = 1
+
+while senha != senha_solic:
+    if tentativas >= 3:
+        print("Número de tentativas alcançadas.\n       Usuário bloqueado.")
+        break
+    senha_solic = input(f"Senha errada.\nRestam {3-tentativas} tentativas.\nTente novamente:")
+    tentativas += 1
+else:
+    print("Senha correta")
