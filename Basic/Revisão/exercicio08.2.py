4bcaaf3378cc070f83ccd6ee1728ddc34eb4f929
TURMA-A <angelovinicius123@gmail.com>
2023-09-28 09:49:45 -0300

    Revisão

---
+++ Basic/Revisão/exercicio08.2.py
@@ -0,0 +1,8 @@
+quantidade = int(input("Digite quantos números deseja adicionar: "))
+soma = 0
+
+for x in range(quantidade):
+    num = float(input("Digite um número: "))
+    soma += num
+media = soma / quantidade
+print(f"{media}")
