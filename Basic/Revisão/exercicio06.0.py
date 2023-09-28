7985e8c03f0750cfa3f8d31bfef873894a497093
TURMA-A <angelovinicius123@gmail.com>
2023-09-28 09:16:12 -0300

    Revisão

--- Basic/Revisão/exercicio06.0.py
+++ Basic/Revisão/exercicio06.0.py
@@ -1,8 +1,8 @@
 num = float(input("Digite um número: "))
 
-if num > 10:
-    print(f"É MAIOR QUE 10!")
-elif num < 10:
-    print(f"NÃO É MAIOR QUE 10!")
-else:
+if num == 10:
     print(f"É IGUAL A 10!")
+elif num > 10:
+    print(f"É MAIOR QUE 10!")
+else:
+    print(f"NÃO É MAIOR QUE 10!")
--- Basic/Revisão/exercicio06.1.py
+++ Basic/Revisão/exercicio06.1.py
@@ -3,12 +3,12 @@ decisao = "s"
 while decisao == "s" or decisao == "S":
     num = float(input("Digite um número: "))
 
-    if num > 10:
-        print(f"É MAIOR QUE 10!")
-    elif num < 10:
-        print(f"NÃO É MAIOR QUE 10!")
-    else:
+    if num == 10:
         print(f"É IGUAL A 10!")
+    elif num > 10:
+        print(f"É MAIOR QUE 10!")
+    else:
+        print(f"NÃO É MAIOR QUE 10!")
     decisao = input("Deseja continuar? (s/n) \n")
 else:
     print(f"Programa encerrado.")
