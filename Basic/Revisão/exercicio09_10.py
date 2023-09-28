fa68514af09ddee7e30ef743b064471c05750783
TURMA-A <angelovinicius123@gmail.com>
2023-09-28 10:52:32 -0300

    Revisão

---
+++ Basic/Revisão/exercicio09_10.py
@@ -0,0 +1,9 @@
+num = int(input("Digite um número: "))
+
+antecessor = num - 1
+sucessor = num + 1
+
+
+print(f"O número antecessor de {num} é {antecessor}.")
+
+print(f"O número sucessor de {num} é {sucessor}.")
