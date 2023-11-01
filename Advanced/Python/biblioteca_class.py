2e42bcd90570bc2d505c430fe3df6d5ea226fd1d
TURMA-A <angelovinicius123@gmail.com>
2023-11-01 11:47:24 -0300

    Att

--- Advanced/Python/biblioteca_class.py
+++ Advanced/Python/biblioteca_class.py
@@ -1,3 +1,57 @@
+class Pessoa:
+
+    def __init__(self, n, i, p, c=False, d=False, a=False):
+        self.nome = n
+        self.idade = i
+        self.peso = p
+        self.comendo = c
+        self.dormindo = d
+        self.andando = a
+
+    def andar(self):
+        if self.andando == False and self.comendo == False and self.dormindo == False:
+            self.andando = True
+            print(f"{self.nome} foi andar. . .")
+        elif self.andando == True:
+            print(f"{self.nome} ainda está andando")
+
+    def parar_andar(self):
+        if self.andando == True:
+            self.andando = False
+            print(f"{self.nome} terminou de andar.")
+        else:
+            print(f"{self.nome} não está andando.")
+
+    def comer(self, comida):
+        if self.andando == False and self.comendo == False and self.dormindo == False:
+            self.comida = comida
+            self.comendo = True
+            print(f"{self.nome} foi comer {self.comida}")
+        elif self.comendo == True:
+            print(f"Termine sua comida antes de comer novamente.")
+
+    def parar_comer(self):
+        if self.comendo == True:
+            self.comendo = False
+            print(f"{self.nome} terminou de comer.")
+        else:
+            print(f"{self.nome} não está comendo.")
+
+    def dormir(self):
+        if self.andando == False and self.comendo == False and self.dormindo == False:
+            self.dormindo = True
+            print(f"{self.nome} foi dormir. . .")
+        elif self.dormindo == True:
+            print(f"{self.nome} já está dormindo")
+
+    def acordar(self):
+        if self.dormindo == True:
+            self.dormindo = False
+            print(f"{self.nome} acordou.")
+        else:
+            print(f"{self.nome} não está dormindo.")
+
+
 class ContaBancaria:
 
     def __init__(self, num_c, nome_c, tipo_c):
@@ -10,12 +64,14 @@ class ContaBancaria:
 
     def depositar(self, deposito):
         if self.statusconta == True:
-            if self.limite > self.limite:
+            if self.saldo < 0:
                 self.saldo += deposito
+                self.limite += (deposito - self.saldo)
+                print(f"Depósito efetuado com sucesso.\n"
+                      f"Você possui R${self.saldo} e R${self.limite} de limite.")
             else:
                 self.saldo += deposito
-                self.limite += deposito - self.saldo
-            print(f"Depósito efetuado com sucesso.\n"
+                print(f"Depósito efetuado com sucesso.\n"
                   f"Você possui R${self.saldo} e R${self.limite} de limite.")
         else:
             print("Por favor ative sua conta antes de usar.")
@@ -26,6 +82,8 @@ class ContaBancaria:
                 self.saldo -= saque
                 if self.saldo < 0:
                     self.limite += self.saldo
+                    print(f"Saque efetuado com sucesso.\n"
+                          f"Você possui R${self.saldo} e R${self.limite} de limite.")
                 else:
                     print(f"Saque efetuado com sucesso.\n"
                           f"Você possui R${self.saldo} e R${self.limite} de limite.")
